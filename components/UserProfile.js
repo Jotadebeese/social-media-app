b673debdddf0cc0258f05dbe7042851069000135
Jotadebeese <jotadebeese@gmail.com>
2023-04-11 19:11:35 +1000

    Added component for the user profile

---
+++ components/UserProfile.js
@@ -0,0 +1,10 @@
+// UI component for user profile
+export default function UserProfile({ user }) {
+    return (
+        <div className="box-center">
+            <img src={user.photoURL || '/default.png'} className="card-img-center" />
+            <p><i>@{user.username}</i></p>
+            <h1>{user.displayName || 'Anonymous User'}</h1>
+        </div>
+    )
+}
