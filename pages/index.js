9cac369f30087c8234d6b8053ae0527f31ae2bfe
Jotadebeese <jotadebeese@gmail.com>
2023-04-14 21:30:24 +1000

    Added functionality to load all posts on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,71 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
-
+import PostFeed from '@/components/PostFeed'
 import Loader from '@/components/Loader'
-import toast from 'react-hot-toast';
+import { orderBy, where, query, collectionGroup, limit, getDocs, Timestamp, startAfter } from 'firebase/firestore';
+import { firestore, postToJSON } from '@/lib/firebase';
+import { useState } from 'react';
 
-export default function Home() {
+
+// Max post to query per page
+const LIMIT = 1;
+
+export async function getServerSideProps(context) {
+  const ref = collectionGroup(firestore, 'posts');
+  const postsQuery = query(
+    ref,
+    where('published', '==', true),
+    orderBy('createdAt', 'desc'),
+    limit(LIMIT),
+  )
+
+  const posts = (await getDocs(postsQuery)).docs.map(postToJSON);
+
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  // Get next page in pagination query
+  const getMorePosts = async () => {
+    setLoading(true)
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? Timestamp.fromMillis(last.createdAt) : last.createdAt;
+
+    const ref = collectionGroup(firestore, 'posts');
+    const postsQuery = query(
+      ref,
+      where('published', '==', true),
+      orderBy('createdAt', 'desc'),
+      startAfter(cursor),
+      limit(LIMIT)
+    )
+
+    const newPosts = (await getDocs(postsQuery)).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <Loader show />
-      <button onClick={() => toast.success('Love you my vida!!!!')}>
-        Toast Me
-      </button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+
+      <div className='box-center'>
+
+        {!loading && !postsEnd && <button className='load-more' onClick={getMorePosts}>Load more</button>}
+        <Loader show={loading} />
+        {postsEnd && 'You have reached the end!'}
+
+      </div>
+    </main>
   )
 }
