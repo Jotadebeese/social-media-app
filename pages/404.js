68d795c710e7388726d92da4792a61b86a2caf41
Jotadebeese <jotadebeese@gmail.com>
2023-04-18 15:05:12 +1000

    added 404 page for page not found

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function Custom404() {
+  return (
+    <main className='box-404'>
+        <div>
+            <h2>
+                404 - nah, 
+                <br/>that page does not exist.
+            </h2>
+            <Link href="/">
+                <button className="load-more">Go back!</button>
+            </Link>
+        </div>
+        <img src={'./404-page.png'} />
+    </main>
+  );
+}
