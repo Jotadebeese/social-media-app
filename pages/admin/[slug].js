512c057dd30d636d90ade28d7d909786fb0ea328
Jotadebeese <jotadebeese@gmail.com>
2023-04-28 21:31:45 +1000

    Started to build the admin/slug page

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -1,7 +1,28 @@
-export default function AdminPostsPage({ }) {
+import AuthCheck from "@/components/AuthCheck";
+import { auth, firestore } from "@/lib/firebase";
+import { doc } from "firebase/firestore";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useDocumentDataOnce } from "react-firebase-hooks/firestore";
+
+export default function AdminPostsPage(props ) {
     return (
-        <main>
-            <h1>Edit AdminPostsPage</h1>
-        </main>
-    )
+        <AuthCheck>
+            <PostManager />
+        </AuthCheck>
+    );
 }
+
+function PostManager() {
+    const [preview, setPreview] = useState(false);
+
+    const router = useRouter();
+    const { slug } = router.query;
+}
+
+const postRef = doc(firestore, 'users', auth.currentUser.uid, 'posts', slug);
+const [post] = useDocumentDataOnce(postRef);
+
+return (
+    <main className={styles.container}></main>
+)
