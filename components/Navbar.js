ad9180ea755d85322fda4a3b2b26470dfef4b939
Jotadebeese <jotadebeese@gmail.com>
2023-05-01 22:06:32 +1000

    Added functionality to reload webpage when sign out ot avoid error

--- components/Navbar.js
+++ components/Navbar.js
@@ -39,7 +39,7 @@ export default function Navbar() {
                             </Link>
                         </li>
                         <li>
-                            <button className="btn-orange" onClick={() => signOut(auth)}>Sign Out</button>
+                            <button className="btn-orange" onClick={() => {signOut(auth); window.location.reload();}}>Sign Out</button>
                         </li>
                         <li>
                             <Link href={`/${username}`}>
@@ -66,7 +66,7 @@ export default function Navbar() {
                         </Link>
                     </li>
                     <li>
-                        <button className="btn-orange" onClick={() => signOut(auth)}>Sign Out</button>
+                        <button className="btn-orange" onClick={() => {signOut(auth); window.location.reload();}}>Sign Out</button>
                     </li>
                     </>
                 )}
